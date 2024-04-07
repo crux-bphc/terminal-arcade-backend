329339c5ecdccb6db9194fd242955b43d52538de
Advik Sinha <advsinha112@gmail.com>
2024-04-07 23:36:13 +0530

    feat: add user route

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 from fastapi import FastAPI
 from api.games import router as games_router
+from api.user import router as user_router
 from firebase_config import db
 
 app = FastAPI()
 
 app.include_router(games_router)
+app.include_router(user_router)
 
 @app.get("/")
 def read_root():
