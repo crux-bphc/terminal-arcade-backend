14140aa30f527c6dce1f9120d89756b4ef00ba9b
Aditya Pentyala <adityapentyala02@gmail.com>
2024-04-07 23:17:16 +0530

    feat: add get_user method

--- api/user.py
+++ api/user.py
@@ -14,4 +14,10 @@ async def create_user(email_id: User):
     db.collection("users").document().set({"email_id" : email_id.email_id})
     return {"message": "e-mail id written succesfully"}
 
-
+@router.get("/users/{user_id}")
+async def get_user(user_id: str):
+    user = db.collection("users").document(user_id).get()
+    if user.exists:
+        return user.to_dict()
+    else:
+        return {"message" : "user not found"}
