774e3fb287516626bd6f26eee349146e50c083c5
Aditya Pentyala <adityapentyala02@gmail.com>
2024-04-07 23:27:23 +0530

    feat: add get_all_users method

--- api/user.py
+++ api/user.py
@@ -9,7 +9,7 @@ router = APIRouter()
 class User(BaseModel):
     email_id: str
 
-@router.post("/user")
+@router.post("/users")
 async def create_user(email_id: User):
     db.collection("users").document().set({"email_id" : email_id.email_id})
     return {"message": "e-mail id written succesfully"}
@@ -21,3 +21,9 @@ async def get_user(user_id: str):
         return user.to_dict()
     else:
         return {"message" : "user not found"}
+
+@router.get("/users")
+async def get_all_users():
+    users = db.collection("users").stream()
+    all_users = [{document.id: document.to_dict()} for document in users]
+    return all_users
