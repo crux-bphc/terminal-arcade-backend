5e81a121d7e598a65358a33d5165780468f9ba5c
Advik Sinha <advsinha112@gmail.com>
2024-04-08 17:53:19 +0530

    chore: switch to different firebase project

--- firebase_config.py
+++ firebase_config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate('key.json')
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'techweek-terminal-arcade-69211.appspot.com'
+    'storageBucket': 'terminalarcade-e6910.appspot.com'
 })
 
 db = firestore.client()
