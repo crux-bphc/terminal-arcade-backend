1fd460cad057207c784654e1867f4eae9c0a43ee
Advik Sinha <advsinha112@gmail.com>
2024-04-08 17:54:09 +0530

    feat: add CORS middleware

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.games import router as games_router
 from api.user import router as user_router
 from firebase_config import db
 
 app = FastAPI()
+
+origins = [
+    "https://terminal-arcade.vercel.app/",
+    "http://localhost:3000"  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(games_router)
 app.include_router(user_router)
