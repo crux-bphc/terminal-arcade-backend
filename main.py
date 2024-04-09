7e4c590673877cb921fc87544a7ee8c473895e9c
Advik Sinha <advsinha112@gmail.com>
2024-04-09 19:09:42 +0530

    feat: add middleware to protect routes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from api.middleware import auth_middleware
 from api.games import router as games_router
 from api.user import router as user_router
 from api.login import router as login_router
@@ -25,6 +26,8 @@ app.include_router(games_router)
 app.include_router(user_router)
 app.include_router(login_router)
 app.include_router(brython_router)
+
+app.middleware("http")(lambda request, call_next: auth_middleware(request, call_next)) 
 
 @app.get("/")
 def read_root():
