ca3b5fbf499fce8a8298ff885414654c555382cc
Swas.py <codewithswastik@gmail.com>
2024-04-11 15:18:28 +0530

    fix: Fix CORS on hanger routes

--- api/middleware.py
+++ api/middleware.py
@@ -11,7 +11,13 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 async def auth_middleware(request: Request, call_next: Callable):
     try:
-        if request.url.path not in ("/login", "/verify-otp"):
+        if request.method == "OPTIONS":
+            return await call_next(request)
+        
+        if request.url.path.startswith("/hanger"):
+            return await call_next(request)
+        
+        if request.url.path not in ("/login", "/verify-otp", "/docs", "/openapi.json"):
             token = request.headers.get("Authorization")
             if token is None:
                 return Response(content = json.dumps({"detail": "Invalid authentication credentials"}), status_code = 401)
@@ -19,6 +25,7 @@ async def auth_middleware(request: Request, call_next: Callable):
             email = payload.get('sub')
             if email is None:
                 return Response(content = json.dumps({"detail": "Invalid authentication credentials"}), status_code = 401)
+        
     except Exception as e:
         return Response(content = json.dumps({"detail": "Invalid authentication credentials"}), status_code = 401)
     return await call_next(request)
