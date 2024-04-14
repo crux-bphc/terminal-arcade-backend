a688c0d257ae4eb0bc14b08efcce5885904c1df7
Advik Sinha <advsinha112@gmail.com>
2024-04-14 15:02:09 +0530

    chore: make storage but env var

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+
+storageBucket = os.getenv('STORAGE_BUCKET')
 
 cred = credentials.Certificate('key.json')
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'terminalarcade-e6910.appspot.com'
+    'storageBucket': storageBucket
 })
 
 db = firestore.client()
