87d7bc1d83bcb2e09cd1ce281d923ffbc7f7790c
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2025-04-09 22:10:25 +0000

    refactor: do url path and method checks outside the try except block

--- api/middleware.py
+++ api/middleware.py
@@ -11,41 +11,42 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 async def auth_middleware(request: Request, call_next: Callable):
+    if request.method == "OPTIONS":
+        return await call_next(request)
+
+    if request.url.path.startswith("/hanger"):
+        return await call_next(request)
+
+    if request.url.path in (
+        "/login",
+        "/verify-otp",
+        "/docs",
+        "/openapi.json",
+        "/creator_leaderboard",
+        "/player_leaderboard",
+    ):
+        return await call_next(request)
+
     try:
-        if request.method == "OPTIONS":
-            return await call_next(request)
+        token = request.headers.get("Authorization")
+        if token is None:
+            return Response(
+                content=json.dumps(
+                    {"detail": "Invalid authentication credentials"}
+                ),
+                status_code=401,
+            )
 
-        if request.url.path.startswith("/hanger"):
-            return await call_next(request)
-
-        if request.url.path not in (
-            "/login",
-            "/verify-otp",
-            "/docs",
-            "/openapi.json",
-            "/creator_leaderboard",
-            "/player_leaderboard",
-        ):
-            token = request.headers.get("Authorization")
-            if token is None:
-                return Response(
-                    content=json.dumps(
-                        {"detail": "Invalid authentication credentials"}
-                    ),
-                    status_code=401,
-                )
-
-            assert SECRET_KEY is not None
-            payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-            email = payload.get("sub")
-            if email is None:
-                return Response(
-                    content=json.dumps(
-                        {"detail": "Invalid authentication credentials"}
-                    ),
-                    status_code=401,
-                )
-
+        assert SECRET_KEY is not None
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        email = payload.get("sub")
+        if email is None:
+            return Response(
+                content=json.dumps(
+                    {"detail": "Invalid authentication credentials"}
+                ),
+                status_code=401,
+            )
     except Exception:
         return Response(
             content=json.dumps({"detail": "Invalid authentication credentials"}),
