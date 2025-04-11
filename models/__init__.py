60d86c265098a67e4ec07f9a72acb8ffc2cedca5
skoriop <karthikp.ivy@gmail.com>
2025-04-11 00:26:18 +0000

    fix: the DB DELETING ALL DATAgit add -u  im killing myself

--- models/__init__.py
+++ models/__init__.py
@@ -29,7 +29,6 @@ from .leaderboard import DbLeaderboardEntry
 
 async def create_db_and_init():
     async with engine.begin() as conn:
-        await conn.run_sync(SQLBase.metadata.drop_all)
         await conn.run_sync(SQLBase.metadata.create_all)
 
 
