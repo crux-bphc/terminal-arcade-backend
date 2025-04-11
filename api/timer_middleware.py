8a89438f8db44560aafe331a325f9e98ebcec65f
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2025-04-11 06:32:20 +0000

    fix: fix timezone in timer

--- api/timer_middleware.py
+++ api/timer_middleware.py
@@ -3,13 +3,14 @@ from fastapi import Request, Response
 from typing import Callable
 import json
 from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
-
+tz = ZoneInfo('Asia/Calcutta')
 SECRET_KEY = os.getenv("SECRET_KEY")
-start_time = datetime(2025, 4, 11, 21, 0, 0, 0)
+start_time = datetime(2025, 4, 11, 21, 0, 0, 0,tzinfo=tz)
 
 async def timer_middleware(request: Request, call_next: Callable):
-    t = datetime.now()
+    t = datetime.now(tz)
     time_left = start_time - t
     if time_left > timedelta(seconds=0):
         return Response(
